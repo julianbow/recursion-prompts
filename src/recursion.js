cc11955390457b686fe0f36e215d39d1185a8703
Julian Bowman <julianfbowman@gmail.com>
2021-02-09 10:06:31 -0700

    Finished sum

--- src/recursion.js
+++ src/recursion.js
@@ -25,6 +25,19 @@ var factorial = function(n) {
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+    // input: array of ints
+    // output: every element in the array added together
+    // create a sum variable
+    var arraySum = 0;
+    // base case: if array is empty
+      // if empty return 0
+      if (array.length === 0) {
+          return 0;
+      } else {
+          arraySum = array[0] + sum(array.slice(1));
+      }
+      // return array sum
+      return arraySum;
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
